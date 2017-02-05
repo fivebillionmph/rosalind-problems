008b42df4b8f3dd3343243c26a4d9bf60e52dcd8
James Go <jamescookgo@gmail.com>
2017-02-04 20:21:01 -0500

    finished rna problem

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Practicing C by doing problems from the Rosalind website: http://rosalind.info/problems/list-view/
 
 Most of these will not be the most efficient way to solve the problem, just doing them to brush up on C.
+
+check for leaks: valgrind --leak-check=full -v ./program-name
--- rna/src/main.c
+++ rna/src/main.c
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[]) {
     char* dna_seq = argv[1];
     int length = strlen(dna_seq);
-    char* rna_seq = (char*) malloc(sizeof(char) + 1);
+    char* rna_seq = (char*) malloc(sizeof(char) * (length + 1));
     strcpy(rna_seq, dna_seq);
     for(int i = 0; i < length; i++) {
         if(rna_seq[i] == 'T') rna_seq[i] = 'U';
